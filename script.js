a93e09a4f27ef088121adcdf4a021cd1199e9598
98aj <103376356+98aj@users.noreply.github.com>
2023-06-15 21:09:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+let button = document.getElementById('addTodoBtn');
+      
+      
+      
+      button.addEventListener('click', ()=>{
+        let orderList = document.getElementById('todoList');
+        let todo = document.getElementById('newTodoInput');
 
+        if(todo.value === ''){
+          alert('Please Enter Text')
+          return;
+        }
+        let tag = document.createElement('li');
+        let text = document.createTextNode(todo.value)
+
+        tag.appendChild(text);
+
+        orderList.appendChild(tag);
+
+        todo.value = '';
+        
+      })
